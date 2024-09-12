0b0d8585af2a524309c631cee5f8dc2d7e8ce631
capprinha <gutoklein21@gmail.com>
2024-09-12 18:05:45 -0300

    correção

--- main.js
+++ main.js
@@ -1,40 +1,50 @@
 const dd = parseInt(prompt("Qual o DD??"))
 const form = (document.getElementById("form"));
-const numero = []
 let linhas = "";
+const nome = (document.getElementById("nome"));
+const telefone = (document.getElementById("tel"));
+const arrayLista = [];
 
 
 
 
-//função principal
+
+
+//função principal\\
 form.addEventListener("submit", function(e){
     e.preventDefault();
     atualizarTabela();
     corpoTabela();
 })
 
+
+
+//função para atualizar a tabela\\
+
 function atualizarTabela(){
-    const nome = (document.getElementById("nome"));
-    const telefone = (document.getElementById("tel"));
-
-    /*if(numero.includes(telefone.value)){
-        alert("Este numero ja foi add a agenda")
+    const telefoneExistente = arrayLista.find(function (item) {
+        return item.telefone == telefone.value;
+    });
+    if(telefoneExistente) {
+        alert("Esse número ja foi cadastrado: " + telefoneExistente.telefone)
     }
-    else*/
-
+    else{ 
     let linha = `<tr>`;
-
     linha += `<td>${nome.value}</td>`;
     linha += `<td>(${dd}) ${telefone.value}</td>`;
     linha += `</td>`;
     linhas += linha;
 
-    nome.value = "";
-    telefone.value = [""];
+    arrayLista.push({ telefone: telefone.value, nome: nome.value })
+    console.log(arrayLista)
     
+    nome.value = "";
+    telefone.value = "";
+    
+    }
 }
 
-
+//função para inserir na tabela\\
 function corpoTabela(){ 
     const corpoTabela = (document.querySelector("tbody"));
     corpoTabela.innerHTML = linhas;
